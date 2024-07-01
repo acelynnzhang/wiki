658829243f60e18d6b6f2833416fd506b67b40e0
Acelynn Zhang <azyx0987e@gmail.com>
2024-07-01 15:23:51 -0500

    end of pthread

---
+++ parallel.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+struct page {
+  string name;
+  int id;
+  map<string, int> words;
+  int pglength;
+  page(string name_, vector<string> intomap) : name(name_) {
+    pglength = intomap.size();
+    for (auto i : intomap) {
+      string temp;
+      for (auto j: i) {
+        if (int(j) >= 65 && int(j) <= 90) {
+          temp.push_back(tolower(j));
+        } else if (int(j) >= 97 && int(j) <= 122) {
+          temp.push_back(j);
+        }
+      }
+      if (temp.empty()) {
+        //cout<< i <<endl;
+      } else if (temp != "or" && temp!= "the" && temp!= "and" && temp!= "a" && temp!= "with" && temp!= "that" && temp!= "an" && temp!= " References" && temp!= "Category" &&  temp!= "or" &&  temp!= "we" && temp!= "is" && temp!= "can" &&temp!= "in" && temp!= "to") {
+        words[temp]++;
+        //cout<<i<<endl;
+      }
+      
+      
+    }
+  }
+};
+
+class wiki {
+ public:
+  wiki(const vector<string>& pagenames);
+    vector<vector<float>> getgrid() { return grid; }
+    vector<string> retruneight();
+    
+ protected:
+  pair<vector<int>, float> bfs(vector<int> start, float tsum);
+  pair<vector<int>, float> bfsnarrow(vector<int> start, float tsum, vector<int> select);
+  string extract_word(string name);
+  vector<float> gridd;
+  vector<vector<float>> grid;  // 2d grid of intersections(same words) of pages
+  vector<string> getpage(const string &pagename);
+  int sizee;
+  vector<page*> allpages;
+  int mynum = 7;
+  void compare(vector<vector<int>> grid, string pagename);
+  void *pg_init(void * arg);
+  static void* do_pg_init(void * arg) {
+    return static_cast<wiki*>(arg)->pg_init(arg);
+  }
+};
