e9b6260033ea5bfeeb42fd24f03bf15a2b2d8076
Acelynn Zhang <azyx0987e@gmail.com>
2024-07-02 16:12:30 -0500

    looks prettier but slower all round

--- parallel.cpp
+++ parallel.cpp
@@ -1,56 +1,8 @@
-// download list of nouns
-// be able to pull up wiki page into readable form
-
 #include "parallel.h"
 
 using namespace std;
 
-void wiki::getgrid() {
-  stringstream s;
-  s << '[';
-  for (auto row : grid) {
-    s << '[';
-    for (auto entry : row) {
-      s << entry << ",";
-    }
-    s << ']';
-  }
-  s << ']';
-  cout << s.str() << endl;
-}
-
-string remove_punct(const string& str) {
-  string ret;
-  remove_copy_if(str.begin(), str.end(), std::back_inserter(ret),
-                 [](int c) { return std::ispunct(c); });
-  return ret;
-}
-
-vector<string> wiki::getpage(const string& pagename) {
-  ifstream words(pagename);
-  vector<string> out;
-  if (words.is_open()) {
-    std::istream_iterator<string> word_iter(words);
-    while (!words.eof()) {
-      out.push_back(remove_punct(*word_iter));
-      ++word_iter;
-    }
-  } else {
-    cout << "file " << pagename << " failed to open" << endl;
-  }
-
-  return out;
-}
-
-string wiki::extract_word(string prev_name) {
-  string name;
-  for (int m = 11; m < prev_name.size() - 4; m++) {
-    name.push_back(tolower(prev_name.at(m)));
-  }
-  return name;
-}
-
-void* wiki::pg_init(arg_struct* args) {
+void* parallelbfs::pg_init(arg_struct* args) {
   string page_name = args->pgname;
   int id = args->id;
   string noun = extract_word(page_name);
@@ -61,7 +13,7 @@ void* wiki::pg_init(arg_struct* args) {
   return NULL;
 }
 
-wiki::wiki(const vector<string>& pagenames) {
+parallelbfs::parallelbfs(const vector<string>& pagenames) {
   num_pgs = pagenames.size();
   grid.resize(num_pgs);
   allpages.resize(num_pgs);
@@ -69,29 +21,27 @@ wiki::wiki(const vector<string>& pagenames) {
   for (size_t k = 0; k < num_pgs; k++) {
     // cout<< pagenames[k] << endl;
     struct arg_struct* args = new arg_struct(k, pagenames[k]);
-    threads[k] = thread(&wiki::pg_init, this, args);
+    threads[k] = thread(&parallelbfs::pg_init, this, args);
   }
   for (int i = 0; i < num_pgs; i++) {
     threads[i].join();
   }
 
-  for (int i = 0; i < num_pgs; i++) {
-    cout << '[' << allpages[i]->noun << " : " << allpages[i]->id << ']' << endl;
-  }
-
-  // grid[0].resize(num_pgs);
-  // compare(grid[0], allpages[1]);
-
+  // for (int i = 0; i < num_pgs; i++) {
+  //   cout << '[' << allpages[i]->noun << " : " << allpages[i]->id << ']' <<
+  //   endl;
+  // }
   for (auto pg : allpages) {
     int i = pg->id;
-    threads[i] = thread(&wiki::compare, this, pg);
+    threads[i] = thread(&parallelbfs::compare, this, pg);
   }
 
   for (int i = 0; i < num_pgs; i++) {
     threads[i].join();
   }
 }
-void wiki::compare(page* curr) {
+
+void parallelbfs::compare(page* curr) {
   grid[curr->id].resize(num_pgs);
   for (auto i : allpages) {  // other pages
     if (i->noun != curr->noun) {
@@ -119,57 +69,24 @@ void wiki::compare(page* curr) {
   }
 }
 
-vector<float> addvecf(vector<float> fir, vector<float> sec) {
-  vector<float> toreturn;
-  for (int i = 0; i < max(fir.size(), sec.size()); i++) {
-    toreturn.push_back(fir[i] + sec[i]);
-  }
-  return toreturn;
-}
+// vector<float> addvecf(vector<float> fir, vector<float> sec) {
+//   vector<float> toreturn;
+//   for (int i = 0; i < max(fir.size(), sec.size()); i++) {
+//     toreturn.push_back(fir[i] + sec[i]);
+//   }
+//   return toreturn;
+// }
 
-float sumvec(vector<float> v) {
-  float toreturn;
-  for (auto i : v) {
-    if (i != static_cast<float>(INT32_MAX)) {
-      toreturn += i;
-    }
-  }
-  return toreturn;
-}
+// float sumvec(vector<float> v) {
+//   float toreturn;
+//   for (auto i : v) {
+//     if (i != static_cast<float>(INT32_MAX)) {
+//       toreturn += i;
+//     }
+//   }
+//   return toreturn;
+// }
 
-vector<string> wiki::retruneight() { atomic<bool> atmarr[num_pgs]; }
+// vector<string> wiki2::retruneight() { atomic<bool> atmarr[num_pgs]; }
 
-void wiki::bfs_on_lvl(vector<int> start, vector<bool>visited) {
-  
-  
-
- }
-
-int main() {
-  vector<string> inputwords;
-  // filter_out_noun("./wiki-nouns", "./onlynoun");
-  //  int count = 0;
-  string path = "./onlynoun";
-  int count = 10;
-  for (const auto& entry : filesystem::directory_iterator(path)) {
-    count--;
-    inputwords.push_back(entry.path());
-    // cout << entry.path() << endl;
-    if (count == 0) {
-      break;
-    }
-  }
-
-  if (inputwords.size() == 0) {
-    return 0;
-  }
-
-  wiki mywiki = wiki(inputwords);
-  mywiki.getgrid();
-
-  // vector<string> out = mywiki.retruneight();
-  // for (int i = 0; i < out.size(); i++) {
-  //   cout << "@ i " << i << " = " << out[i] << endl;
-  // }
-  return 1;
-}
+void parallelbfs::bfs_on_lvl(vector<int> start, vector<bool> visited) {}
--- parallel.h
+++ parallel.h
@@ -1,68 +1,19 @@
-#pragma once
+#ifndef PARALLEL_H
+#define PARALLEL_H
 
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <filesystem>
-#include <queue>
-#include <thread>
-#include <atomic>
+#include "wiki.h"
 
 using namespace std;
-struct page {
-  string noun;
-  int id;
-  map<string, int> words;
-  int pglength;
-  page(string noun_, vector<string> intomap) : noun(noun_) {
-    pglength = intomap.size();
-    for (auto i : intomap) {
-      string temp;
-      for (auto j: i) {
-        if (int(j) >= 65 && int(j) <= 90) {
-          temp.push_back(tolower(j));
-        } else if (int(j) >= 97 && int(j) <= 122) {
-          temp.push_back(j);
-        }
-      }
-      if (temp.empty()) {
-        //cout<< i <<endl;
-      } else if (temp != "or" && temp!= "the" && temp!= "and" && temp!= "a" && temp!= "with" && temp!= "that" && temp!= "an" && temp!= " References" && temp!= "Category" &&  temp!= "or" &&  temp!= "we" && temp!= "is" && temp!= "can" &&temp!= "in" && temp!= "to") {
-        words[temp]++;
-        //cout<<i<<endl;
-      }
-      
-      
-    }
-  }
-};
 
-struct arg_struct {
-  int id;
-  string pgname;
-  arg_struct(int id_, string pgname_): id(id_), pgname(pgname_){};
-};
-
-class wiki {
+class parallelbfs : public wiki {
  public:
-  wiki(const vector<string>& pagenames);
-    void getgrid();
-    vector<string> retruneight();
-    
+  parallelbfs(const vector<string>& pagenames);
+
  protected:
-  void wiki::bfs_on_lvl(vector<int> start, vector<bool> visited);
-  string extract_word(string name);
+  void bfs_on_lvl(vector<int> start, vector<bool> visited);
   vector<pair<vector<int>, float>> best_with_start;
-  vector<vector<float>> grid;  // 2d grid of intersections(same words) of pages
-  vector<string> getpage(const string &pagename);
-  int num_pgs;
-  vector<page*> allpages;
-  int mynum = 7;
   void compare(page* page);
-  void *pg_init(arg_struct* args);
+  void* pg_init(arg_struct* args);
 };
+
+#endif
